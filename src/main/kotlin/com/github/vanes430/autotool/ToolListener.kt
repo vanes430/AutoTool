db5b947d80d18eafbc8c39a68edd3bde2616491b
Shimazu <71627068+vanes430@users.noreply.github.com>
2025-07-22 07:42:07 +0000

    sneaking feature

--- src/main/kotlin/com/github/vanes430/autotool/ToolListener.kt
+++ src/main/kotlin/com/github/vanes430/autotool/ToolListener.kt
@@ -4,22 +4,22 @@ import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
-import org.bukkit.event.player.PlayerAnimationEvent
-import org.bukkit.event.player.PlayerAnimationType
+import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.event.block.Action
 
 class ToolListener(private val plugin: AutoTool) : Listener {
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    fun onPlayerAnimate(event: PlayerAnimationEvent) {
-        if (event.animationType != PlayerAnimationType.ARM_SWING) return
+    fun onPlayerInteract(event: PlayerInteractEvent) {
+        if (event.action != Action.LEFT_CLICK_BLOCK) return
 
         val player = event.player
+        if (player.isSneaking) return // Jangan auto tool saat sneak
         if (!plugin.isAutoToolOn(player)) return
 
-        val targetBlock = player.getTargetBlock(null as? Set<Material>, 5) ?: return
+        val block = event.clickedBlock ?: return
+        if (block.type == Material.AIR) return
 
-        if (targetBlock.type == Material.AIR) return
-
-        plugin.autoTool(player, targetBlock)
+        plugin.autoTool(player, block)
     }
 }
